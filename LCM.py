df854fe4ef491ca8227d18f8fd991c74d776054b
shweta0312jain-pranjal <shweta0312jain@gmail.com>
2025-11-14 10:13:20 +0530

    Add files via upload

---
+++ LCM.py
@@ -0,0 +1,14 @@
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+
+a, b = num1, num2
+
+while b != 0:
+    a, b = b, a % b
+
+gcd = a
+lcm = (num1 * num2) // gcd
+
+print("LCM is:", lcm)
